c58a87a9a8cf41695ed6114476c18423dabd4a3d
AlexaP2022 <alexapunzalan@gmail.com>
2022-06-09 19:41:21 -0700

    added javascript functionality to recipe results

--- assets/script.js
+++ assets/script.js
@@ -1,10 +1,12 @@
 var formEl = document.querySelector("#user-form");
-var recipeResults = document.querySelector("#recipe-results");
+var recipeResults = document.querySelector("#recipe-results"); //article div with ID of recipe-results
 //Edamam API
 var endpointURL = "https://api.edamam.com/search?";
 var appKey = "71d09702eb91ca96b0d97199f6a9702a";
 var appID = "9b63d55f";
-//Second API - Need to find one!
+//Second API Kroger Grocery Store Product Search
+    //What search parameters do we want? (brand, term, location) - https://developer.kroger.com/reference/#operation/productGet
+    //var endpointURL2 = "https://api.kroger.com/v1/products?filter.brand={{BRAND}}&filter.term={{TERM}}&filter.locationId={{LOCATION_ID}}"
 
 var submitBtn = document.querySelector("#search-btn");
 var clearBtn = document.querySelector("#clear-btn");
@@ -20,7 +22,8 @@ function getAPI(event) {
             console.log(data);
             displayRecipes(data.hits); // feeds data specific to matching recipes to the display recipes function
         });
-         // getting input from the input tag id 
+    //local storage//
+    // getting input from the input tag id 
     var input = document.getElementById('q').value;
 
     // setting items to a key and value in local storage
@@ -42,25 +45,34 @@ function getAPI(event) {
     searchHistory.appendChild(ingredientsList);
 
     // need it to stay on page
+
+    //make ingredients list clickable so you can retrive that same search result
+
+    //link ingredients list from Edamam API to pull shopping list from kroger API
+
 }
 
 function displayRecipes(fourRecipes) {
     for (var i = 0; i < 4; i++) {
         console.log(fourRecipes[i])
         var card = document.createElement("div");
-        var recipeName = document.createElement('h4');
+        var recipeName = document.createElement('a');
         var recipeImage = document.createElement('img');
         var recipeServes = document.createElement('p');
         var healthLabels = document.createElement('p');
-        recipeName.innerText = fourRecipes[i].recipe.label;
-        recipeServes.innerText = "Servings: " + fourRecipes[i].recipe.yield;
-        healthLabels.innerText = fourRecipes[i].recipe.healthLabels[0,1,2,3,4,5,6,7]; // returns some health labels we did not ask for
+        var ingredientCount = document.createElement('p');
         var recipeImageLocation = fourRecipes[i].recipe.image;
+        recipeName.innerText = fourRecipes[i].recipe.label;
+        recipeName.setAttribute("href", fourRecipes[i].recipe.url);
+        recipeServes.innerText = "Servings: " + fourRecipes[i].recipe.yield;
+        healthLabels.innerText = fourRecipes[i].recipe.healthLabels[0,1,2,3,4,5,6,7]; // returns only one label at a time per recipe, not specifying returns all labels
+        ingredientCount.innerText = "Ingredient count: " + fourRecipes[i].recipe.ingredientLines.length;
         console.log(recipeImageLocation) // is a valid URL to recipe image
         card.appendChild(recipeName);
         card.appendChild(recipeServes);
         card.appendChild(healthLabels);
-        card.appendChild(recipeImage); // figure out how to wrap an anchor tag around the appended image so that you can follow the link  to the recipe.
+        card.appendChild(ingredientCount);
+        card.appendChild(recipeImage); 
         recipeImage.setAttribute("src", recipeImageLocation);
         recipeResults.appendChild(card);
     }
@@ -76,12 +88,13 @@ formEl.addEventListener("submit", getAPI);
 // var searchResults = document.getElementById('ingredient-search-history');
 
 // saving the input to local storage 
+
 // appending the input from local storage to the ingredients search history div
 // function saveIngredients() {
 
-   
+// code to allow user to type in new ingredient searches without refreshing the page
 
-// }
+
 
 
 
